56106988296df465db58cfb82a5dff0cc2e8d14f
Rajiv Gupta <rajiv.gupta1.contractor@pepsico.com>
2024-05-28 12:43:15 +0530

    Added User and Video modal

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,7 +60,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generaeAccessToken = function () {
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -74,7 +74,7 @@ userSchema.methods.generaeAccessToken = function () {
     }
   );
 };
-userSchema.methods.generaeRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
           _id: this._id
